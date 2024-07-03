9237428205661b99bf90ab1f3a85f815782423bb
leifehetland <lehetland1@gmail.com>
2024-07-03 13:11:45 -0400

    npm run build and npm run test working as expected

---
+++ src/arithmetic.ts
@@ -0,0 +1,23 @@
+// A constructor Arithmetic is created.
+export default class Arithmetic {
+
+  // Constructor Arithmetic is given a sum method that returns two numbers added together.
+  sum = (a: number, b:number) => {
+    return a + b
+  };
+
+  // Constructor Arithmetic is given a difference method that returns two numbers subtracted.
+  difference = (a: number, b:number) => {
+    return a - b
+  };
+
+  // Constructor Arithmetic is given a product method that returns two numbers multiplied together.
+  product = (a: number, b:number) => {
+    return a * b
+  };
+
+  // Constructor Arithmetic is given a quotient method that returns two numbers divided.
+  quotient = (a: number, b:number) => {
+    return a / b
+  };
+};
